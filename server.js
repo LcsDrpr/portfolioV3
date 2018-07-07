b960fa2bfdddac129e9b300b0b77b7a7134367d7
Lucas Drapier <lucasdrapier@MacBook-Pro-de-Lucas.local>
2018-07-07 13:06:03 +0200

    update

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const app = express();
 app.use(compression());
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static('/dist'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/home.html'));
+  res.sendFile(path.join('/dist/home.html'));
 });
 
 // Start the app by listening on the default Heroku port
