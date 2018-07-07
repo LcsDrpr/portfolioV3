1014c5988c90a87c40e6a67ad388ed9ce5747f31
Lucas Drapier <lucasdrapier@MacBook-Pro-de-Lucas.local>
2018-07-07 13:33:16 +0200

    update json server

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+'/dist/home.html'));
 });
+app.get('/*', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
+  });
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
