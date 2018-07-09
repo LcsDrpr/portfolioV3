c15bcbe00c1041a4026eeb18d594d4d1a3c1dcaf
Lucas Drapier <lucasdrapier@MacBook-Pro-de-Lucas.local>
2018-07-09 14:01:54 +0200

    routes

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(compression());
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/dist/home.html'));
+app.get('/', function(req,res) {
+    res.sendFile(path.join(__dirname+'/dist/home.html'));
 });
-app.get('/*', function(req,res) {
+app.get('/cv', function(req,res) {
     res.sendFile(path.join(__dirname+'/dist/index.html'));
-  });
+});
 
 
 // Start the app by listening on the default Heroku port
