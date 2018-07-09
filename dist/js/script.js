83e0c49f93857a21ecfa339a1c524ea16b7062f6
Lucas Drapier <lucasdrapier@MacBook-Pro-de-Lucas.local>
2018-07-09 22:18:06 +0200

    update v3

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,7 +4,7 @@ $('document').ready(function() {
     console.log( "ready!" );
     console.log(window.location);
 
-    var adress = "curriculum";
+    var adress = "/curriculum";
 
     var redirect = function Redirect() {
         window.location = adress;
